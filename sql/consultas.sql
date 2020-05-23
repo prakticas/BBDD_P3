af371c482b356e5f73d1ee7fb6a0b4c0c391d7bb
dolansete <57452068+dolansete@users.noreply.github.com>
2020-05-23 19:55:39 +0200

    Update consultas.sql

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -17,7 +17,13 @@ from (select aerolinea, RANK()over(order by por ASC) as rnk
     where t2.rnk <=3
 ;
 /**2**/
-
+SELECT Estado, PORCENTAJE, rnk 
+FROM (SELECT Estado, PORCENTAJE, RANK() over (order by PORCENTAJE DESC) as rnk
+        FROM (SELECT jdesv.Estado, jdesv.desv*100/jtot.tot as PORCENTAJE 
+            FROM (SELECT Estado,COUNT(*) as desv FROM AEROPUERTO join desvio ON Aeropuerto.id=desvio.newaeropuerto GROUP BY Estado) jdesv
+            JOIN
+            (SELECT Estado,COUNT(*) as tot FROM AEROPUERTO join vuelo ON Aeropuerto.id=vuelo.origen GROUP BY Estado) jtot
+            ON  jdesv.Estado=jtot.estado)) where rnk <=3;
 /**3**/
 
 
