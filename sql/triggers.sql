6906142b2ae223c88d15ecd5f39949d3e47e53c5
dolansete <57452068+dolansete@users.noreply.github.com>
2020-05-28 13:07:02 +0200

    Update triggers.sql

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,1 +1,18 @@
 /**Actualizar desvios y retrasos al poner Incidencias**/
+CREATE or REPLACE TRIGGER NEWRETRASO
+BEFORE INSERT ON INCIDENCIA
+FOR EACH ROW
+WHEN (new.tipo = 'retrasado')
+  BEGIN
+  INSERT INTO RETRASO(id) VALUES (:new.id);
+END NEWRETRASO;
+/
+
+CREATE or REPLACE TRIGGER NEWDESVIO
+BEFORE INSERT ON INCIDENCIA
+FOR EACH ROW
+WHEN ((new.tipo = 'desviado1') OR (new.tipo = 'desviado2'))
+  BEGIN
+  INSERT INTO DESVIO(id) VALUES (:new.id);
+END NEWDESVIO;
+/
