c5d38b57e367b0e9eb0ce85ed50e81294ac01c1c
dolansete <57452068+dolansete@users.noreply.github.com>
2020-05-28 13:15:37 +0200

    Update triggers.sql

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -5,6 +5,7 @@ FOR EACH ROW
 WHEN (new.tipo = 'retrasado')
   BEGIN
   INSERT INTO RETRASO(id) VALUES (:new.id);
+  raise_application_error(-20001,'Ahora se debería especificar el tiempo de retraso');
 END NEWRETRASO;
 /
 
@@ -14,5 +15,6 @@ FOR EACH ROW
 WHEN ((new.tipo = 'desviado1') OR (new.tipo = 'desviado2'))
   BEGIN
   INSERT INTO DESVIO(id) VALUES (:new.id);
+  raise_application_error(-20001,'Ahora se debería especificar el nuevo avion y nuevo aeropuerto');
 END NEWDESVIO;
 /
