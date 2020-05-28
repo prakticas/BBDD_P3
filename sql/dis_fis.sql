f14406adc6f5e0be7fc8cc86e8afea6a044b3e99
Íñigo Aréjula Aísa <arejula27@gmail.com>
2020-05-28 15:47:57 +0200

    tabla incret

--- sql/dis_fis.sql
+++ sql/dis_fis.sql
@@ -8,3 +8,18 @@ PARTITION q3_desviado ('desviado')
 
 CREATE INDEX desv_aer ON Desvio(newaeropuerto);
 CREATE INDEX inc_vuel ON incidencia(vuelo);
+
+CREATE TABLE INCRET (
+	Id Number(11),
+	VUELO REFERENCES VUELO(IDV) ON DELETE CASCADE,
+    Tiempo Number(11),
+    PRIMARY KEY (Id),
+	Foreign key(Id) references incidencia(id)
+);
+
+INSERT INTO INCRET(ID,VUELO,Tiempo)
+select id,vuelo, Tiempo
+from 
+incidencia
+natural join 
+retraso;
