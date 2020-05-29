1569279e3e8d69357b0c4522b74d8d3a8ab353c3
dolansete <57452068+dolansete@users.noreply.github.com>
2020-05-29 11:38:33 +0200

    Update triggers.sql

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,25 +1,3 @@
-/**Actualizar desvios y retrasos al poner Incidencias**/
-SET SERVEROUTPUT ON
-
-CREATE or REPLACE TRIGGER NEWRETRASO
-AFTER INSERT ON INCIDENCIA
-FOR EACH ROW
-WHEN (new.tipo = 'retrasado')
-  BEGIN
-  INSERT INTO RETRASO(id) VALUES (:new.id);
-  DBMS_OUTPUT.put_line ('Ahora se debería especificar el tiempo de retraso');
-END NEWRETRASO;
-/
-
-CREATE or REPLACE TRIGGER NEWDESVIO
-AFTER INSERT ON INCIDENCIA
-FOR EACH ROW
-WHEN ((new.tipo = 'desviado1') OR (new.tipo = 'desviado2'))
-  BEGIN
-  INSERT INTO DESVIO(id) VALUES (:new.id);
-  DBMS_OUTPUT.put_line ('Ahora se debería especificar el nuevo avion y nuevo aeropuerto');
-END NEWDESVIO;
-/
 
 /**NO BORRAR DE DESVIO**/
 CREATE or REPLACE TRIGGER NOBORRARDES
@@ -99,3 +77,25 @@ en las dos tablas y se puede hacer join de dicha fila*/
 /**solo hace falta borrar al deletar de desvio, ya que de incidencia esta en cascade*/
 
 
+/**Actualizar desvios y retrasos al poner Incidencias**/
+SET SERVEROUTPUT ON
+
+CREATE or REPLACE TRIGGER NEWRETRASO
+AFTER INSERT ON INCIDENCIA
+FOR EACH ROW
+WHEN (new.tipo = 'retrasado')
+  BEGIN
+  INSERT INTO RETRASO(id) VALUES (:new.id);
+  DBMS_OUTPUT.put_line ('Ahora se debería especificar el tiempo de retraso');
+END NEWRETRASO;
+/
+
+CREATE or REPLACE TRIGGER NEWDESVIO
+AFTER INSERT ON INCIDENCIA
+FOR EACH ROW
+WHEN ((new.tipo = 'desviado1') OR (new.tipo = 'desviado2'))
+  BEGIN
+  INSERT INTO DESVIO(id) VALUES (:new.id);
+  DBMS_OUTPUT.put_line ('Ahora se debería especificar el nuevo avion y nuevo aeropuerto');
+END NEWDESVIO;
+/
